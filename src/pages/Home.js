70ba7e925d1fc7425912991477550667bd036ec3
BelaM80 <mueller.bela@gmx.de>
2020-11-04 15:10:34 +0100

    Add json api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
+import { getLists } from '../api/list';
 
-function Add() {
+const Home = () => {
+  const [lists, setLists] = useState(null);
+
+  useEffect(async () => {
+    const newLists = await getLists();
+    setLists(newLists);
+  }, []);
   return (
     <div>
       Home
-      <Link to="/béla">Béla&apos;s Wishlist</Link>
-      <Link to="/laura">Laura&apos;s Wishlist</Link>
+      {lists?.map((list) => (
+        <Link key={list.id} to={`/${list.title}`}>
+          {list.title}
+        </Link>
+      ))}
       <Button>
         <Link to="/add">+</Link>
       </Button>
     </div>
   );
-}
+};
 
-export default Add;
+export default Home;
